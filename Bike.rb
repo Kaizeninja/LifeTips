bd053445c3c967b0f5c45713b1a470cfc6954d39
Martin McReynolds <martinmcrey@gmail.com>
2019-08-06 20:39:41 +0000

    works

---
+++ Bike.rb
@@ -0,0 +1,15 @@
+class Car
+    attr_accessor :model, :color
+
+    def initialize(model, color)
+      @model = model
+      @color = color
+    end
+
+    def output_car
+      puts "That's a #{@color} #{@model} car."
+    end
+end
+
+car = Car.new(:Mustang, :black)
+car.output_car
