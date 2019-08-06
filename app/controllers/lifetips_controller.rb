9fa26d2b12086437f875311041c9d07eb1b001f7
martin mcreynolds <martinmcrey@gmail.com>
2019-08-05 21:35:04 -0500

    quiz 2

--- app/controllers/lifetips_controller.rb
+++ app/controllers/lifetips_controller.rb
@@ -1,6 +1,22 @@
 class LifetipsController < ApplicationController
 
 def index
+   @lifetips = Lifetip.all 
 end
+
+def new 
+   @lifetip = Lifetip.new 
+end 
+
+def create
+    Lifetip.create(lifetip_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def lifetip_params
+    params.require(:lifetip).permit(:name, :description)
+  end
 
 end
